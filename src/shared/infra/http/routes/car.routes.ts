a7c6b8e5a3290961a62045481cef4ffdee36bcb7
Fredson Chaves <fredsonchaves07@gmail.com>
2021-05-17 13:10:23 -0300

    refact: bcrypt js configuration

--- src/shared/infra/http/routes/car.routes.ts
+++ src/shared/infra/http/routes/car.routes.ts
@@ -1,11 +1,13 @@
 import { CreateCarController } from '@modules/cars/useCases/createCar/CreateCarController'
 import { Router } from 'express'
+import { ensureAuthenticated } from '@shared/infra/http/middlewares/ensureAuthenticated'
+import { ensureAdmin } from '@shared/infra/http/middlewares/ensureAdmin'
 
 
 const carRoutes = Router()
 
 const createCarController = new CreateCarController()
 
-carRoutes.post('/', createCarController.handle)
+carRoutes.post('/', ensureAuthenticated, ensureAdmin, createCarController.handle)
 
 export { carRoutes }
