17c8c5492bb99a430e1c6680080b013c41265a31
Fredson Chaves <fredsonchaves07@gmail.com>
2021-05-29 14:29:33 -0300

    feat: create tests and rental cases use

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -1,3 +1,4 @@
+import { Rental } from '@modules/rentals/infra/typeorm/entities/Rental';
 import { IRentalsRepository } from '@modules/rentals/infra/typeorm/repositories/IRentalsRepository';
 import { AppError } from './../../../../shared/errors/AppError';
 
@@ -15,7 +16,7 @@ class CreateRentalUseCase{
         private rentalsRepository: IRentalsRepository
     ){}
 
-    async execute({ user_id, car_id, expected_return_date }: IRequest): Promise<void>{
+    async execute({ user_id, car_id, expected_return_date }: IRequest): Promise<Rental>{
         const carUnavailable = await this.rentalsRepository.findOpenRentalByCar(car_id)
 
         if(carUnavailable){
@@ -27,6 +28,14 @@ class CreateRentalUseCase{
         if(rentalOpenToUser){
             throw new AppError('Ther is a rental in progress for user!');   
         }
+
+        const rental = await this.rentalsRepository.create({
+            user_id, 
+            car_id, 
+            expected_return_date
+        })
+
+        return rental
     }
 }
 
